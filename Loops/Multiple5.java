596c1445cc7c80795e8d03207069251b2b953c29
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-03-06 16:33:24 +0530

    Add files via upload

---
+++ Loops/Multiple5.java
@@ -0,0 +1,9 @@
+public class Multiple5{
+public static void main(String[] args){
+int num= 6;
+for(int i = 1;i<=10;i++){
+int result = num*i;
+System.out.println(num + " * " + i + " = " + result);
+}
+}
+}
