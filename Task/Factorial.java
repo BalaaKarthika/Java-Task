be89c592539d4ba6e46a6e3a7995f2ea01bf8fc3
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-03-31 16:20:11 +0530

    Add files via upload

--- Task/Factorial.java
+++ Task/Factorial.java
@@ -1,9 +1,12 @@
-public class Factorial{
-public static void main(String[] args){
-int n=5;
-for(int i=1;i<=n;i++)
-{
-System.out.println(n*=i);
-}
-}
+// find a factorial
+
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5; 
+        int fact = 1;
+        for (int i = 1; i <= number; i++) { 
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + number + " is " + fact); 
+    }
 }
