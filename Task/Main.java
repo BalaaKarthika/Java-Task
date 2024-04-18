d3364a5e2a3e4a764427b3d173a31335254c4867
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-04-18 12:22:58 +0530

    Add files via upload

---
+++ Task/Main.java
@@ -0,0 +1,12 @@
+// constructor
+
+public class Main{
+int x; // create a class attribute 
+public Main(){
+x=5;
+}
+public static void main(String[] args){
+Main obj = new Main();
+System.out.println(obj.x);
+}
+}
