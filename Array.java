4e7f538b8ee536cd0b07d4dfeaa21149063bc99f
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-07-18 14:19:51 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array{
+ public static void main(String[] args){
+   int[] arr =  new int[50];
+   arr[0] = 1;
+   arr[2] = 3;
+    for(int i= 0;i<5;i++)
+    {
+      System.out.println(arr[i]);
+    }
+}
+}
