e18764485dc227e954905adcabf0729fcf31a1a0
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-08-07 22:08:02 +0530

    Add files via upload

---
+++ Task/Swap.java
@@ -0,0 +1,22 @@
+// Write a java program to swap two numbers without using third variable
+
+public class Swap{
+ public static void main(String[] args){
+ int x = 10, y = 20;
+ System.out.println(" Before Swapping");
+ System.out.println(x);
+ System.out.println(y);
+ x=x+y;
+ y=x-y;
+ x=x-y;
+ System.out.println("After Swapping");
+ System.out.println(x);
+ System.out.println(y);
+}
+}
+
+
+
+
+ 
+ 
