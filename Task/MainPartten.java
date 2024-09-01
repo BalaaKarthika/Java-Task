3e3656089a329d47445cfe805bdc7c3e446decb5
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-09-01 19:49:00 +0530

    Add files via upload

---
+++ Task/MainPartten.java
@@ -0,0 +1,13 @@
+// parten 
+
+public class MainPartten{
+public static void main(String[] arge){
+  int row = 5;
+  for(int i =1;i<=row;i++){
+     for(int j =1;j<=i;j++){
+	  System.out.print(j+" ");
+	  }
+	   System.out.println(" " );
+	  }
+	  }
+	  }
