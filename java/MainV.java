d2ba9f748103c996288b8d4c1e1349b04af0a034
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-09-01 19:47:27 +0530

    Add files via upload

---
+++ java/MainV.java
@@ -0,0 +1,14 @@
+public class MainV{
+    public static void main(String[] args){
+	  //int age =19;
+	  Age V = new Age;
+	  int a = 10;
+	  System.out.println(a);
+	  V.display();
+	  
+	 }
+	 public void display(){
+	 int b =20;
+	 System.out.println(b);	
+	}
+}	
