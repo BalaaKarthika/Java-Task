0bdc324e24151a5f88a982f59a26386519eb7687
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-09-03 18:23:48 +0530

    Add files via upload

---
+++ Task/Numbers.java
@@ -0,0 +1,14 @@
+public class Numbers{
+  public static void main(String[] args){
+  int count = 1;
+  int row = 4;
+  for(int i= 1;i<=row;i++){
+    for(int j=1;j<=i;j++){
+	 System.out.print(j+ " ");
+	 //Count++;
+	}
+	System.out.println();
+	}
+	}
+	}
+	
