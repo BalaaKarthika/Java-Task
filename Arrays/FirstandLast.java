2a5213558a5a737acb69af1f4a54af634944f7e3
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-09-04 21:46:04 +0530

    Add files via upload

---
+++ Arrays/FirstandLast.java
@@ -0,0 +1,12 @@
+//display the first and last element of array
+
+public class FirstandLast{
+   public static void main(String[] args){
+   int arr[] ={2,3,5,7,9,3,4};
+   int n = arr.length;
+   int first = arr[0];
+   int last = arr[n-1];
+   System.out.println("First element is:"+ first);
+   System.out.println("Last element is:"+ last);
+   }
+  } 
