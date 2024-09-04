fb4fa897d83b7b557d646a1c8cb929dc051b8609
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-09-04 21:41:49 +0530

    Add files via upload

---
+++ Arrays/FistElement.java
@@ -0,0 +1,9 @@
+//display the first element of array
+
+public class FistElement{
+   public static void main(String[] args){
+   int arr[] ={2,3,5,7,9,3,4};
+   int first = arr[0];
+   System.out.println("First element is:"+ first);
+   }
+  } 
