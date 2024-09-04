533b34d43b7408718403151b4adbd88a9bd642df
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-09-04 21:33:55 +0530

    Add files via upload

---
+++ Arrays/GivenArray.java
@@ -0,0 +1,8 @@
+//Find the length of given array
+
+public class GivenArray{
+   public static void main(String[] args){
+   int arr[]  = { 2,4,5,6,8,2,1};
+   System.out.println("Length is:"+ arr.length);
+   }
+   }
